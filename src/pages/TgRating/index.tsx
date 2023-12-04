34eb47d61c641de20f578be260ecc28b0326c9eb
Nurmukhammad <sobirovn707@gmail.com>
2023-12-04 16:43:23 +0500

    rating page sync

--- src/pages/TgRating/index.tsx
+++ src/pages/TgRating/index.tsx
@@ -8,8 +8,6 @@ import RateStars from "src/components/RatingStars";
 import { useRemoveParams } from "src/hooks/custom/useCustomNavigate";
 import { TelegramApp } from "src/utils/tgHelpers";
 
-const Stars = [{}];
-
 const TgRating = () => {
   const { id } = useParams();
   const removeParams = useRemoveParams();
@@ -37,7 +35,7 @@ const TgRating = () => {
   }, [rate]);
 
   return (
-    <div className="absolute inset-0 bg-mainGray flex flex-col">
+    <div className="absolute inset-0 bg-mainGray flex flex-col z-[1000]">
       <Header title={`Заказ №${id}`} subTitle={`Статус: `}>
         <div
           onClick={() => TelegramApp.closeWindow()}
@@ -51,28 +49,46 @@ const TgRating = () => {
 
       <div className="flex flex-1 flex-col px-4">
         <RateStars value={rate} onChange={handleRate} className="flex-[2]" />
-
         <h5 className="font-bold text-xl text-[rgba(0,_0,_0,_0.33)] justify-center items-center flex flex-1 mt-5">
           {renderTitle}
         </h5>
+        {!rate && (
+          <h4 className="font-bold text-xl justify-center items-center text-center flex flex-1 mb-5">
+            Оставьте свой отзыв и помогите нам стать ещё лучше
+          </h4>
+        )}
 
-        <h4 className="font-bold text-xl justify-center items-center text-center flex flex-1 mb-5">
-          {rate < 5
-            ? "Что вас разочаровало?"
-            : "При желании можете добавить комментарии"}
-        </h4>
-
-        <BaseInput className="flex flex-1 flex-col">
-          <MainInput
-            className="!bg-darkGray"
-            register={register("comment")}
-            placeholder={"Ваш комментарий..."}
+        {!rate && (
+          <img
+            src="/assets/images/safia.jpg"
+            alt=""
+            className="w-28 opacity-20 mx-auto mb-10"
           />
-        </BaseInput>
+        )}
+
+        {rate > 0 && (
+          <h4 className="font-bold text-xl justify-center items-center text-center flex flex-1 mb-5">
+            {rate < 5
+              ? "Что вас разочаровало?"
+              : "При желании можете добавить комментарии"}
+          </h4>
+        )}
+
+        {!!rate && (
+          <BaseInput className="flex flex-1 flex-col">
+            <MainInput
+              className="!bg-darkGray"
+              register={register("comment")}
+              placeholder={"Ваш комментарий..."}
+            />
+          </BaseInput>
+        )}
       </div>
-      <button type="submit" className="btn btn-primary">
-        Готово
-      </button>
+      {!!rate && (
+        <button type="submit" className="btn btn-primary">
+          Готово
+        </button>
+      )}
     </div>
   );
 };
