05290ecc3a9265388090e453a773216cbe5f7f60
Nurmukhammad <sobirovn707@gmail.com>
2024-03-14 12:20:13 +0500

    logystics excel

--- src/pages/RequestsLogystics/index.tsx
+++ src/pages/RequestsLogystics/index.tsx
@@ -1,7 +1,7 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Departments, MainPermissions, Order } from "@/utils/types";
 import Pagination from "@/components/Pagination";
-import { FC, useState } from "react";
+import { FC, useRef, useState } from "react";
 import dayjs from "dayjs";
 import useOrders from "@/hooks/useOrders";
 import Card from "@/components/Card";
@@ -18,6 +18,7 @@ import EmptyList from "@/components/EmptyList";
 import Loading from "@/components/Loader";
 import { useTranslation } from "react-i18next";
 import { dateTimeFormat, yearMonthDate } from "@/utils/keys";
+import { useDownloadExcel } from "react-export-table-to-excel";
 
 interface Props {
   add: MainPermissions;
@@ -56,6 +57,15 @@ const RequestsLogystics: FC<Props> = ({ add, edit }) => {
   const request_status = useQueryString("request_status");
   const branchJson = useQueryString("branch");
   const branch = branchJson && JSON.parse(branchJson);
+  const tableRef = useRef(null);
+
+  const { onDownload } = useDownloadExcel({
+    currentTableRef: tableRef.current,
+    filename: t("requests_for_logystics"),
+    sheet: t("requests_for_logystics"),
+  });
+
+  const downloadAsPdf = () => onDownload();
 
   const {
     data: requests,
@@ -78,12 +88,13 @@ const RequestsLogystics: FC<Props> = ({ add, edit }) => {
     ...(!!urgent && { urgent: !!urgent }),
   });
 
-  if (orderLoading) return <Loading absolute />;
-
   return (
     <Card>
       <Header title="requests_for_logystics">
-        <button className="btn btn-primary btn-fill mr-2">
+        <button
+          onClick={downloadAsPdf}
+          className="btn btn-primary btn-fill mr-2"
+        >
           {t("export_to_excel")}
         </button>
         {permission?.[add] && (
@@ -99,7 +110,7 @@ const RequestsLogystics: FC<Props> = ({ add, edit }) => {
 
       <div className="table-responsive grid-view content">
         <ItemsCount data={requests} />
-        <table className="table table-hover">
+        <table className="table table-hover" ref={tableRef}>
           <TableHead
             column={column}
             onSort={(data) => $sort(data)}
@@ -107,67 +118,76 @@ const RequestsLogystics: FC<Props> = ({ add, edit }) => {
           >
             <LogFilter />
           </TableHead>
-          <tbody id="requests_body">
-            {!!requests?.items?.length &&
-              !orderLoading &&
-              (sort?.length ? sort : requests?.items)?.map((order, idx) => (
-                <tr className={requestRows(order?.status)} key={idx}>
-                  <td width="40">{handleIdx(idx)}</td>
-                  <td width="80">
-                    {permission?.[edit] ? (
-                      <Link
-                        id="request_id"
-                        to={`/requests-logystics/${order?.id}`}
-                        state={{ prevPath: pathname + search }}
-                      >
-                        {order?.id}
-                      </Link>
-                    ) : (
-                      <span className={"text-link"}>{order?.id}</span>
-                    )}
-                  </td>
+          {orderLoading ? (
+            <Loading absolute />
+          ) : (
+            <tbody id="requests_body">
+              {!!requests?.items?.length &&
+                !orderLoading &&
+                (sort?.length ? sort : requests?.items)?.map((order, idx) => (
+                  <tr className={requestRows(order?.status)} key={idx}>
+                    <td width="40">{handleIdx(idx)}</td>
+                    <td width="80">
+                      {permission?.[edit] ? (
+                        <Link
+                          id="request_id"
+                          to={`/requests-logystics/${order?.id}`}
+                          state={{ prevPath: pathname + search }}
+                        >
+                          {order?.id}
+                        </Link>
+                      ) : (
+                        <span className={"text-link"}>{order?.id}</span>
+                      )}
+                    </td>
 
-                  <td width={40}>
-                    {order?.fillial?.id ? (
-                      <img src="/assets/icons/home.svg" alt="from-fillial" />
-                    ) : (
-                      <img src="/assets/icons/marker.svg" alt="from-location" />
-                    )}
-                  </td>
+                    <td width={40}>
+                      {order?.fillial?.id ? (
+                        <img src="/assets/icons/home.svg" alt="from-fillial" />
+                      ) : (
+                        <img
+                          src="/assets/icons/marker.svg"
+                          alt="from-location"
+                        />
+                      )}
+                    </td>
 
-                  <td>{order?.user?.full_name}</td>
-                  <td>
-                    <span className={"not-set"}>
-                      {order?.fillial?.parentfillial?.name}
-                    </span>
-                  </td>
-                  <td
-                    className={cl({
-                      ["font-bold"]: order?.category?.urgent,
-                    })}
-                  >
-                    {order?.category?.name}
-                  </td>
-                  <td>
-                    {!order?.category?.urgent ? t("not_urgent") : t("urgentt")}
-                  </td>
-                  <td>{dayjs(order?.created_at).format(dateTimeFormat)}</td>
-                  <td>
-                    {t(
-                      handleStatus({
-                        status: order?.status,
-                        dep: Departments.logystics,
-                      })
-                    )}
-                  </td>
-                  <td>
-                    {!!order?.user_manager
-                      ? order?.user_manager
-                      : t("not_given")}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
+                    <td>{order?.user?.full_name}</td>
+                    <td>
+                      <span className={"not-set"}>
+                        {order?.fillial?.parentfillial?.name}
+                      </span>
+                    </td>
+                    <td
+                      className={cl({
+                        ["font-bold"]: order?.category?.urgent,
+                      })}
+                    >
+                      {order?.category?.name}
+                    </td>
+                    <td>
+                      {!order?.category?.urgent
+                        ? t("not_urgent")
+                        : t("urgentt")}
+                    </td>
+                    <td>{dayjs(order?.created_at).format(dateTimeFormat)}</td>
+                    <td>
+                      {t(
+                        handleStatus({
+                          status: order?.status,
+                          dep: Departments.logystics,
+                        })
+                      )}
+                    </td>
+                    <td>
+                      {!!order?.user_manager
+                        ? order?.user_manager
+                        : t("not_given")}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          )}
         </table>
         {isFetching && <Loading absolute />}
         {!requests?.items?.length && !orderLoading && <EmptyList />}
