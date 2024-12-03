ed3c394d025df5db0026bb695953243c4b1fab29
Nurmukhammad <sobirovn707@gmail.com>
2024-12-03 15:27:03 +0500

    tool validation

--- src/webApp/components/FreezerClientItem/index.tsx
+++ src/webApp/components/FreezerClientItem/index.tsx
@@ -6,7 +6,8 @@ import {
   incrementSelected,
 } from "@/store/reducers/webInventory";
 import { useAppDispatch, useAppSelector } from "@/store/utils/types";
-import { CSSProperties, useState } from "react";
+import { Spin } from "antd";
+import { CSSProperties, useEffect, useState } from "react";
 
 type Props = {
   style?: CSSProperties;
@@ -18,18 +19,24 @@ const FreezerClientItem = ({ style, tool }: Props) => {
   const cart = useAppSelector(cartSelector);
   const [tool_id, $tool_id] = useState<number>();
 
-  const { data: tool_info } = getFreezerBalances({
+  const { data: tool_info, isFetching } = getFreezerBalances({
     enabled: !!tool_id,
     id: tool_id!,
   });
 
-  const addToCart = (id: number) => {
-    dispatch(addItem(tool));
+  const addToCart = async (id: number) => {
     $tool_id(id);
+    const my_tool = await tool_info;
+    // if (!!my_tool?.amount) dispatch(addItem(tool));
   };
 
+  useEffect(() => {
+    if (tool_id === tool_info?.tool?.id && !!tool_info?.amount)
+      dispatch(addItem(tool));
+  }, [tool_info, tool_id]);
+
   const handleIncrement = () => {
-    if (tool_info?.amount && tool_info?.amount > cart[tool.id].count)
+    if (tool_info?.amount && tool_info?.amount > cart?.[tool.id]?.count)
       dispatch(incrementSelected(tool.id?.toString()));
     else if (!tool_info?.amount)
       dispatch(incrementSelected(tool.id?.toString()));
@@ -44,39 +51,47 @@ const FreezerClientItem = ({ style, tool }: Props) => {
       <div className="flex flex-1 justify-between pt-3 flex-col">
         <div className="flex flex-1 justify-center ml-4 flex-col">
           <h5 className="font-bold text-base">{tool?.name}</h5>
-          {tool_info?.tool?.id && tool_id === tool.id && (
+          {tool_info && tool_id === tool.id && (
             <p className="text-red-400 text-xs">
-              Остаток - {tool_info?.amount}
+              {!!tool_info?.amount
+                ? `Остаток - ${tool_info?.amount}`
+                : "Этот товар пока нет на складе"}
             </p>
           )}
         </div>
 
         <div className="bg-tgPrimary flex items-center justify-center rounded-tl-3xl w-20 h-9 self-end">
-          {!cart[tool.id] ? (
-            <button
-              className="flex flex-1 justify-center"
-              onClick={() => addToCart(+tool.id)}
-            >
-              <img src="/icons/cart.svg" alt="cart" height={20} width={20} />
-            </button>
+          {!isFetching ? (
+            !cart[tool.id] ? (
+              <button
+                className="flex flex-1 justify-center"
+                onClick={() => addToCart(+tool.id)}
+              >
+                <img src="/icons/cart.svg" alt="cart" height={20} width={20} />
+              </button>
+            ) : (
+              <div className="flex justify-evenly flex-1  text-white ">
+                <button
+                  className="flex flex-1 justify-center"
+                  onClick={() =>
+                    dispatch(decrementSelected(tool?.id?.toString()))
+                  }
+                >
+                  -
+                </button>
+                <span className="flex flex-1 justify-center">
+                  {cart[tool.id]?.count}
+                </span>
+                <button
+                  className="flex flex-1 justify-center"
+                  onClick={handleIncrement}
+                >
+                  +
+                </button>
+              </div>
+            )
           ) : (
-            <div className="flex justify-evenly flex-1  text-white ">
-              <button
-                className="flex flex-1 justify-center"
-                onClick={() => dispatch(decrementSelected(tool.id?.toString()))}
-              >
-                -
-              </button>
-              <span className="flex flex-1 justify-center">
-                {cart[tool.id].count}
-              </span>
-              <button
-                className="flex flex-1 justify-center"
-                onClick={handleIncrement}
-              >
-                +
-              </button>
-            </div>
+            <Spin size="small" />
           )}
         </div>
       </div>
