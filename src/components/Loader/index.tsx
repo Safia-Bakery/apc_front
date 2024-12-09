1cf749fa317e24b97bc4e7d75436c4e32897ef57
Nurmukhammad <sobirovn707@gmail.com>
2024-12-09 11:22:44 +0500

    loading animation changed

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react";
 import styles from "./index.module.scss";
 import cl from "classnames";
-import safiaLogo from "/images/safia.png";
+// import safiaLogo from "/images/safia.png";
+import { Spin } from "antd";
 
 interface Props {
   is_static?: boolean;
@@ -13,13 +14,14 @@ const Loading: FC<Props> = ({ is_static = false, className }) => {
     <div
       className={cl(className, styles.wrap, { [styles.absolute]: !is_static })}
     >
-      <img
+      {/* <img
         className={styles.loadingCircle}
         src={safiaLogo}
         height={50}
         width={50}
         alt="loading..."
-      />
+      /> */}
+      <Spin size="large" />
     </div>
   );
 };
